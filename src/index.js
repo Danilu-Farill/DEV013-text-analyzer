c5280451b3fbcc74b7e0d01d09f911355dbc0108
Gladis Danilu Farill <danilu080893@hotmail.com>
2023-12-08 09:24:16 -0600

    Modoficación del html y del css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const input = document.getElementsByName("user-input");
+const button = document.getElementById("reset-button");
+
+button.addEventListener("click",reset);
+
+
+function reset() {
+    input.value="";
+    console.log(input);
+}
+/*function time() {
+    console.log("hola");    
+}
+setTimeout(time, 4000);
+let clock = function(){
+    let date = new Date()
+    console.log(`${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`);
+}
+
+setInterval(clock, 4000);*/
