254a533959d9d65ce0c0526b3bd60d3ba15030f1
Gladis Danilu Farill <danilu080893@hotmail.com>
2023-12-18 00:14:46 -0600

    LAS PRIMERAS 3 FUNCIONES REALIZADAS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,41 +1,43 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      console.log(text.element.value);
-
-      
-    }
+    let word = text.split(" ").length;
+    return  word;
   },
   getCharacterCount: (text) => {
+    let character = text.length;//cuenta los caracteres con espacio
+    return character;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro 
+    /*let noSpace = letras;
+    console.log(noSpace.replace(/\s+/g, '').length);//sin espacio
+    console.log(noSpace.split(" ").join("").length);//sin espacio*/
+    //Falta excluir signos
+    let noSpace = text.split(" ").join("").length;
+    return noSpace;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    /*let longitud = character/word;
+    console.log(longitud);/*///RECTIFICAR
+    let lengthMedium =  text.split(" ").join("").length/text.split(" ").length;
+    return lengthMedium;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let matchNumber = /(\d+)/g;
+    console.log(matchNumber);
+    return text.match(matchNumber).length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    console.log(text);
+    let counter = 0;
+    //for (let index = 0; index < text.length; index++) {
+      //if()
+   // }
   },
 };
 
 export default analyzer;
-
-/*
-for(let i = 0; i < array.length; i++ ){
-
-}
-*/
-/* for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      text.element;
-    } */
-    //function reset() {
-      //console.log(input.value.length);
-  //}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,80 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 const button = document.getElementById("reset-button");
-const input = document.querySelector("textarea[name='user-input']");
-//const input = document.getElementById("user-input");
+const input = document.querySelector("[name='user-input']");
+const wordCount = document.querySelector("[data-testid='word-count']");
+const characterCount = document.querySelector("[data-testid='character-count']");
+const characterNoSpacesCount= document.querySelector("[data-testid='character-no-spaces-count']");
+const numberCount = document.querySelector("[data-testid= 'number-count']");
+const numberSum = document.querySelector("[data-testid= 'number-sum']");
+const wordLengthAverage = document.querySelector("[data-testid= 'word-length-average']");
+
 button.addEventListener("click",reseText);
+input.addEventListener("keyup", showText);
 
 function reseText() {
     input.value ="";
+    wordCount. innerHTML += "";
+    wordLengthAverage.innerHTML += "" ;
+    characterCount.innerHTML += "";
+    characterNoSpacesCount.innerHTML += "";
+    numberCount.innerHTML += "";
+    numberSum.innerHTML += "";
+}
+let wordCountPlaceHolder = wordCount.innerHTML;
+let characterCountPlaceHolder = characterCount.textContent;
+let characterNoSpacesCountPlaceHolder = characterNoSpacesCount.innerHTML;
+let wordLengthAveragePlaceHolder = wordLengthAverage.innerHTML; 
+let numberCountPlacerHolder = numberCount.innerHTML;
+let numberSumPlaceHolder = numberSum.innerHTML;
+
+
+function showText(valor) {
+    wordCount.innerHTML = wordCountPlaceHolder + analyzer.getWordCount(valor.target.value);
+    characterCount.textContent = characterCountPlaceHolder + analyzer.getCharacterCount(valor.target.value);
+    characterNoSpacesCount.innerHTML = characterNoSpacesCountPlaceHolder + analyzer.getCharacterCountExcludingSpaces(valor.target.value);
+    wordLengthAverage.innerHTML = wordLengthAveragePlaceHolder + analyzer.getAverageWordLength(valor.target.value);
+    numberCount.innerHTML = numberCountPlacerHolder + analyzer.getNumberCount(valor.target.value);
+    numberSum.innerHTML = numberSumPlaceHolder + analyzer.getNumberSum(valor.target.value);
 }
 
-//const textInput = 
 
-
-/*function time() {
-    console.log("hola");    
+/*function showText(valor) {//con esto me aparece lo que voy a escribiendo pero se repiten las letras
+    //analyzer.getCharacterCount(valor.target.value);
+    characterCount.textContent = characterCountPlaceHolder + analyzer.getCharacterCount(valor.target.value);
+    //characterCount.textContent = analyzer.getCharacterCount(valor.target.value);
+    characterNoSpacesCount.innerHTML += ` ${valor.target.value}`;
+    numberCount.innerHTML += ` ${valor}`;
+    wordLengthAverage.innerHTML += ` ${valor}`;
+    numberSum.innerHTML+= ` ${valor}`;
 }
-setTimeout(time, 4000);
-let clock = function(){
-    let date = new Date()
-    console.log(`${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`);
+ */
+
+/*
+
+function showText(valor) {//con esto me aparece lo que voy a escribiendo pero se repiten las letras
+    analyzer.getCharacterCount(valor.target.value);
 }
 
-setInterval(clock, 4000);*/
+function textareaValue(valor) {
+    //characterCount.textContent = characterCountPlaceHolder + valor.target.value.length;
+    //characterCount.textContent = analyzer.getCharacterCount(valor.target.value);//esta es modificando el html y css para aparecer individual
+    characterNoSpacesCount.innerHTML += ` ${valor.target.value}`;
+    numberCount.innerHTML += ` ${valor}`;
+    wordLengthAverage.innerHTML += ` ${valor}`;
+    numberSum.innerHTML+= ` ${valor}`;
+    showText();
+}
+*/
+/*
+function wordCountFunction(){
+
+    setTimeout(function() { 
+        let valor = document.querySelector("[name='user-input'");
+        let respuesta = document.querySelector("[data-testid='word-count']");
+        let total = valor.value.length;
+        document.querySelector("[data-testid='word-count']").innerHTML = total;
+       // wordCount.textContent += ` ${input.value.length}`;
+    });
+    }
+*/
